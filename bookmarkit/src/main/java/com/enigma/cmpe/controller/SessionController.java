67ceaf58693f6ffe6daa213c68e465d59f1e75d0
hardikjoshi90 <hardikjoshi90@gmail.com>
2014-05-04 18:34:58 -0700

    Modified signup

--- bookmarkit/src/main/java/com/enigma/cmpe/controller/SessionController.java
+++ bookmarkit/src/main/java/com/enigma/cmpe/controller/SessionController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,29 +19,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.enigma.cmpe.dao.UserDAO;
 import com.enigma.cmpe.dao.sessionDAO;
 import com.enigma.cmpe.domain.login;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 
 @Controller
 @RequestMapping("")
+
 public class SessionController extends HttpServlet {
 
-	private final UserDAO userDAO;
-	// HashMap<String, String> sessionState = new HashMap<String, String>();
-	private final sessionDAO sessionDAO;
+	private static UserDAO userDAO;
 
+	private static sessionDAO sessionDAO;
+
+	@SuppressWarnings("static-access")
 	public SessionController() throws UnknownHostException {
-		MongoClient mongoClient = new MongoClient(new ServerAddress(
-				"localhost", 27017));
-		// final MongoClient mongoClient = new MongoClient(new
-		// MongoClientURI(mongoURIString));
-		final DB blogDatabase = mongoClient.getDB("bookmarkit");
-
-		// blogPostDAO = new BlogPostDAO(blogDatabase);
-		userDAO = new UserDAO(blogDatabase);
-		sessionDAO = new sessionDAO(blogDatabase);
+		
+		userDAO = userDAO.getInstance();
+		sessionDAO = sessionDAO.getInstance();
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
@@ -53,12 +53,11 @@ public class SessionController extends HttpServlet {
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String loginPost(@ModelAttribute("login") login loginObj,
-			Model model, HttpServletResponse response,
+	public String loginPost(Model model, HttpServletResponse response,
 			HttpServletRequest request) throws IOException {
 
-		String username = loginObj.getUsername();
-		String password = loginObj.getPassword();
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
 
 		System.out.println("Username: " + username + " password: " + password);
 		DBObject user = userDAO.validateLogin(username, password);
@@ -66,13 +65,12 @@ public class SessionController extends HttpServlet {
 		if (user != null) {
 
 			// valid user, let's log them in
-			String sessionID = sessionDAO.startSession(user.get("_id")
-					.toString());
+			String sessionID = sessionDAO.startSession(user.get("_id").toString());
 			System.out.println("SessionID: " + sessionID);
 			if (sessionID == null) {
-				response.sendRedirect("/doit/error");
+				response.sendRedirect("/bookmarkit/error");
 			} else {
-				response.sendRedirect("/doit/welcome?token=" + sessionID);
+				response.sendRedirect("/bookmarkit/" + username);
 			}
 
 		}
@@ -84,40 +82,33 @@ public class SessionController extends HttpServlet {
 	public String logoutGet(HttpServletRequest request,
 			HttpServletResponse response,
 			@ModelAttribute("login") login loginObj) throws IOException {
-		String token = loginObj.getToken();
-		System.out.println("logout token is:" + token);
-		if (token == null) {
+		String username = request.getParameter("username");
+		if (username == null) {
 			// no session to end
-			response.sendRedirect("/doit/login");
+			response.sendRedirect("/bookmarkit/login");
 		} else {
 			// deletes from session table
-			sessionDAO.endSession(token);
+			sessionDAO.endSession(username);
 			return "login";
 		}
 
 		return "login";
 	}
 
-	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
-	public String welcome(Model model, @RequestParam("token") String token,
+	@RequestMapping(value = "/{user}", method = RequestMethod.GET)
+	public String welcome(Model model, @PathVariable("user") String username,
 			HttpServletResponse response) throws IOException {
-
-		if (!token.equals(null) && !token.equals("")) {
-			System.out.println("Generated token is:" + token.length());
-			DBObject session = sessionDAO.getSession(token);
+			DBObject session = sessionDAO.getSession(username);
 			if (session == null)
-				response.sendRedirect("/doit/login");
-			model.addAttribute("token", token);
-		} else
-			response.sendRedirect("/doit/login");
+				response.sendRedirect("/bookmarkit/login");
+			model.addAttribute("username", username);
 		return "welcome";
 	}
 	
-	
+
 	@RequestMapping(value = "/fb", method = RequestMethod.GET)
 	public String fblogin(Model model) throws IOException {
 		return "FacebookLogin";
 	}
-	
 
 }
